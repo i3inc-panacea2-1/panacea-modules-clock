0eb4082cde4b213b40ebc779483779f53ca0c311
Portno <j.remoundos@dotbydot.gr>
2019-05-12 18:52:03 +0300

    first commit

---
+++ src/Panacea.Modules.Clock/ClockControl.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Panacea.Controls;
+using Panacea.Multilinguality;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Panacea.Modules.Clock
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class ClockControl : UserControl
+    {
+        private DispatcherTimer t;
+
+
+        public ClockControl()
+        {
+            InitializeComponent();
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            
+            t.Tick -= t_Elapsed;
+            LanguageContext.Instance.LanguageChanged -= Instance_LanguageChanged;
+            t.Stop();
+            t = null;
+        }
+
+        private void UpdateClock()
+        {
+            Text.Text = DateTime.Now.ToString("HH:mm");
+            Text2.Text = DateTime.Now.ToString("ddd MMM d", LanguageContext.Instance.Culture);
+        }
+
+        private void Clock_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            UpdateClock();
+            t = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            t.Tick += t_Elapsed;
+            LanguageContext.Instance.LanguageChanged += Instance_LanguageChanged;
+            Console.WriteLine(GetValue(Material.RelativeFontSizeProperty));
+            t.Start();
+        }
+
+        void Instance_LanguageChanged(object sender, EventArgs e)
+        {
+            UpdateClock();
+        }
+
+        void t_Elapsed(object sender, EventArgs e)
+        {
+            UpdateClock();
+        }
+    }
+}
---
+++ src/Panacea.Modules.Clock/ClockPlugin.cs
@@ -0,0 +1,47 @@
+﻿using Panacea.Core;
+using Panacea.Modularity;
+using Panacea.Modularity.UiManager.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Panacea.Modules.Clock
+{
+    public class ClockPlugin : IPlugin
+    {
+        private readonly PanaceaServices _core;
+        ClockControl _clock;
+
+        public ClockPlugin(PanaceaServices core)
+        {
+            _core = core;
+            _clock = new ClockControl();
+            _clock.SetValue(DockPanel.DockProperty, Dock.Right);
+        }
+
+        public Task BeginInit()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public Task EndInit()
+        {
+            _core.GetUiManager().AddNavigationBarControl(_clock);
+            return Task.CompletedTask;
+        }
+
+        public Task Shutdown()
+        {
+            _core.GetUiManager().RemoveNavigationBarControl(_clock);
+            return Task.CompletedTask;
+        }
+    }
+}
